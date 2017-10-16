69c02c043a3c68df4ccb1fa2ac4b61e8a90db962
talia-walkey <twrox8@gmail.com>
2017-10-15 18:06:45 -0700

    Mixed Yuqi's, Alynna's, and my code

--- beavver.taliawalkey.ca/register.php
+++ beavver.taliawalkey.ca/register.php
@@ -1,3 +1,6 @@
+<?php
+require_once('connect.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -10,9 +13,12 @@
     <!-- Bootstrap -->  
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap-grid.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Arvo|Montserrat:700" rel="stylesheet">
+    
+        <link href="style.css" rel="stylesheet" />
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -89,7 +95,7 @@
              <script>
                  var butClick = document.getElementById('signup-button');
                      butClick.addEventListener("click", function(){
-                         window.location.href = "register.php";
+                         window.location.href = "new.php";
                      });
                  
              </script>
@@ -98,6 +104,71 @@
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
 </nav>
+
+
+
+
+ <div id="topspace" class="container">
+              <div id="regibox">
+                  <p id="imgcenter"><img id="beavver" alt='beavverLogo' src="img/Asset%201@4x.png"/></p>
+                  
+                      <form>
+                      <div class="form-group">
+                          <p class="regititle">New to us? <br/>Sign up now</p>
+                      </div>
+                
+                      <div class="form-group">
+                          <input class="regiinput form-control" type="text"  placeholder="First Name" id='username' name='username'>
+                          <hr class="dashline">
+                      </div>
+                      <div class="form-group">
+                          <input type="text" class="regiinput form-control" placeholder="Last Name">
+                          <hr class="dashline">
+                      </div>
+                      
+                      <div class="form-group">
+                        <input  type="email" class="regiinput form-control" id="exampleInputEmail1" placeholder="Enter email" name='email'>
+                        <hr class="dashline">
+                      </div>
+                      <div class="form-group">
+                        <input type="password" class="regiinput form-control" placeholder="Password" name="password">
+                        <hr class="dashline">
+                      </div>
+                      <div class="form-group">
+                        <input  type="password" class="regiinput form-control" placeholder="Confirm Password">
+                        <hr class="dashline">
+                      </div>
+<!--
+                      <div class="form-check">
+                        <label id="rmbme" class="form-check-label">
+                          <input type="checkbox" class="form-check-input">
+                          Remember Me
+                        </label>
+                      </div>
+                      
+-->
+                      <button type="submit" class="msubmit2 btn btn-primary">
+                          <img id="linkedinbtn" alt='linkedInLogo' src="img/linkedin-logo.svg">
+                          &nbsp;Login with LinkedIn
+                      </button>
+                     
+                      
+                      <br/>
+                      <button type="submit" class="msubmit btn btn-primary" id='submitBut' name='submitBut' >Submit</button>
+                      
+                      <!--formaction="registeredProfile.php"-->
+                       
+                       
+                  </form>
+              </div>
+            </div>
+        
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+
     
     <!-- SEARCH BAR -->  
     <div id="search-box" class="form-row align-items-center">
--- beavver.taliawalkey.ca/registeredProfile.php
+++ beavver.taliawalkey.ca/registeredProfile.php
@@ -1,3 +1,57 @@
+<?php
+require_once('connect.php');
+
+    $servername = "beavver.taliawalkey.ca";
+    $dblogin = "talia185_Beavver";
+    $password = "D3Beavver!";
+    $dbname = "talia185_cc";
+    
+   //var_dump($_POST);
+
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dblogin, $password);
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+       
+      // $sql = "SELECT `username`, `email`, `password` FROM `login` WHERE `username` = '$username'";
+       
+        $sql = "SELECT * FROM login";
+        
+        $statement = $conn->prepare($sql);
+        $statement->execute();
+       
+        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+      
+ echo "<table>";
+        foreach ($rows as $row) {
+            echo "<tr>
+                     <th> </th>
+                     <th> </th>
+                     <th> </th>
+                  </tr>";  
+                  
+            echo "<tr>
+                    <td>" . $row['username'] . "</td>
+                    <td>" . $row['email'] . "</td>
+                    <td>" . $row['password'] . "</td>
+                  </tr";
+        }
+        echo "</table>";
+        
+    
+    } catch(PDOException $e) {
+       // echo "<p style='color: red;'>From the SQL code: $sql</p>";
+        $error = $e->getMessage();
+        //echo "<p style='color: red;'>$error</p>";
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -166,3 +220,4 @@
     <script src="js/header-footer.js"></script>
   </body>
 </html>
+
