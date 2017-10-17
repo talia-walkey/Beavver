e5a0cbbe2f7bdc07cb7d4dbc043f437462b08f94
Alynna Alcira <alynna.alcira@gmail.com>
2017-10-16 20:55:06 -0700

    Created the header and footer php

---
+++ beavver.taliawalkey.ca/footer.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Beavver</title>
+
+    <!-- Bootstrap -->  
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-grid.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<!--    <link href="https://fonts.googleapis.com/css?family=Arvo|Montserrat:700" rel="stylesheet">-->
+    
+<!--    <link href="style.css" rel="stylesheet" />-->
+    <link rel="stylesheet" type="text/css" href="style.css?d=<?php echo time(); ?>" />      
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+    <!-- FOOTER -->
+    <div id="footer">
+        
+        <!-- NEWLETTER -->
+        <div class="container">
+          <div class="row newsletter-row">
+            <div class="col-lg-6">
+              <p>SUBSCRIBE TO NEWSLETTER</p>
+            </div>
+            <div class="col-lg-6">
+                <div id="email-box" class="form-row align-items-center">
+                    <a href="#"><button id="newsletter-button"><span class="glyphicon glyphicon-envelope" id="envelope-icon"></span></button></a>
+                    <input type="text" class="form-control" placeholder="E-mail" id="email-footer-input">  
+                </div>  
+            </div>
+          </div>
+        </div>
+        
+        <hr/>
+        
+        <!-- FOOTER LINKS -->
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+                <div class="navbar-header">
+                  <a class="navbar-brand" href="#">
+                    <img src="img/logo-text-white.png" id="logo" alt="logo">
+                  </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav footer-nav">
+                    <li><a href="#">ABOUT US</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                    <li><a href="#">SUPPORT</a></li>
+                    <li><a href="#">TO BE DEFINED</a></li>
+                  </ul> 
+                </div>
+            </div>  
+        </nav>
+        
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+            </div>
+            <div class="col-lg-8">
+                <p class="copyright-msg">© 2017 Beavver. All rights reserved.</p> 
+            </div>
+            <div class="col-lg-4">
+            </div>
+          </div>
+        </div>
+        
+    </div> <!-- end of footer -->    
+      
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>  
+
+  </body>
+</html>
