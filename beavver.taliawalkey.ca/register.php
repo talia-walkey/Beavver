18d9877878ba63ee3f9d68a01e8b25152ef6ebc7
Alynna Alcira <alynna.alcira@gmail.com>
2017-10-30 13:27:35 -0700

    Fixed the footer in register page (hopefully lol)

--- beavver.taliawalkey.ca/register.php
+++ beavver.taliawalkey.ca/register.php
@@ -112,8 +112,6 @@ require_once('connect.php');
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    
-<div><?php include 'footer.php';?></div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -122,6 +120,9 @@ require_once('connect.php');
     <script src="js/header-footer.js"></script>
     
 </div>    
+          
+<div><?php include 'footer.php';?></div>
+      
     <script>
         $(document).ready(function(){
             document.getElementById("registerForm").addEventListener("submit", function(ev){
