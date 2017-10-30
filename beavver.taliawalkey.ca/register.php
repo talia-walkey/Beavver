324ece4c69ab5a97f3ab3f15eeee476431242307
Alynna Alcira <alynna.alcira@gmail.com>
2017-10-30 13:31:53 -0700

    Got rid of header-footer.js script tag

--- beavver.taliawalkey.ca/register.php
+++ beavver.taliawalkey.ca/register.php
@@ -117,7 +117,6 @@ require_once('connect.php');
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/header-footer.js"></script>
     
 </div>    
           
