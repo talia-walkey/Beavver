21040556601072c377a6c7207ad3b65f6f208031
Alynna Alcira <alynna.alcira@gmail.com>
2017-11-24 12:01:55 -0800

    Added a functional contact form

--- beavver.taliawalkey.ca/contact.php
+++ beavver.taliawalkey.ca/contact.php
@@ -16,6 +16,7 @@
     <link href="https://fonts.googleapis.com/css?family=Arvo|Montserrat:700" rel="stylesheet">
     
     <link rel="stylesheet" type="text/css" href="style.css?d=<?php echo time(); ?>" />   
+    <link rel="stylesheet" type="text/css" href="css/beavver-contact.css?d=<?php echo time(); ?>" />   
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -54,9 +55,73 @@
             <div class="col-md-1"></div>
     		<div class="col-md-10 center-text">
                 <h1>CONTACT</h1>
+                <p>Please complete this form and someone will be in contact with you shortly.</p>
             </div>
             <div class="col-md-1"></div>
         </div>
+        
+        <div class="row contact-section">
+            <div class="col-md-3"></div>
+            <div class="col-md-6 contact-fields">
+                <p class="sub-heading">Send us a message</p>
+                <form method="post" action="contact.php">
+                  <div class="form-row">
+                    <div class="form-group col-md-6">
+                      <label for="inputEmail4">Name</label>
+                      <input name="name" type="text" class="form-control" id="name" placeholder="Name">
+                    </div>
+                      
+                    <div class="form-group col-md-6">
+                      <label for="inputPassword4">Email Address</label>
+                      <input name="email" type="text" class="form-control" id="email" placeholder="Email Address">
+                    </div>
+                      
+                  </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Your Message</label>
+                        <textarea name="message" class="form-control" id="message" rows="3"></textarea>
+                  </div>
+                    
+                <div class="form-group">  
+                  <input type="submit" class="btn primaryBtn" name="submit" value="Submit" id="submit">
+                </div>
+                <div class="form-group">
+                    <!-- To make the contact form functional -->
+                    <?php
+                    if (isset($_POST['name'])) {
+                        $name = $_POST['name'];
+                    }
+
+                    if (isset($_POST['email'])) {
+                        $email = $_POST['email'];
+                    }
+
+                    if (isset($_POST['message'])) {
+                        $message = $_POST['message'];
+                    }
+                    
+                        $from = 'From: Beavver Contact Form'; 
+                        $to = 'alynna.alcira@gmail.com'; 
+                        $subject = 'Beavver Contact Form';
+
+                        $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+                        if (isset($_POST['submit'])) {		
+                            if (mail ($to, $subject, $body, $from)) { 
+                            $result = "Your message has been sent!";
+                            echo $result;
+                        } else { 
+                           $result = "Something went wrong. Please try again!"; 
+                            echo $result;
+                           } 
+                        }
+                    ?>
+                </div>
+                </form>                                
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+        
         
     </div> <!-- END OF CONTENT -->
     
@@ -67,14 +132,10 @@
     <div>
         <?php include 'footer.php';?>
     </div>  
-    
-
       
-
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/header-footer.js"></script>
   </body>
 </html>
