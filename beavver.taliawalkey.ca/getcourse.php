eee47c4579251ea1e7791bfbb4c9e82e0dcdc184
Tess <tesshuangxj@yahoo.com>
2017-12-03 14:22:30 -0800

    edit getcourse
    correct the typo in getcourse.php

--- beavver.taliawalkey.ca/getcourse.php
+++ beavver.taliawalkey.ca/getcourse.php
@@ -7,7 +7,7 @@ $user = $_SESSION['user']['id'];
 $sql1 = "SELECT new_pt_course.crse_numb, new_pt_course.crse_name, new_pt_course.crse_url FROM user_courses
         LEFT JOIN login ON login.id=user_courses.user_id
         LEFT JOIN new_pt_course ON new_pt_course.id=user_courses.courses_id
-        WHERE user_courses.id = '$user'";
+        WHERE user_courses.user_id = '$user'";
 
         $statement = $conn->prepare($sql1);
         $statement->execute();
