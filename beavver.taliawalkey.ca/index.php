7501b0d3a7443902a147da3c62e415c2be80a1e6
Alynna Alcira <alynna.alcira@gmail.com>
2017-12-04 17:02:10 -0800

    Removed jquery script tag so collapse will work

--- beavver.taliawalkey.ca/index.php
+++ beavver.taliawalkey.ca/index.php
@@ -386,7 +386,7 @@ require_once('connect.php');
     </script>  
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
   </body>
