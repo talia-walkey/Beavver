24e70235368571087ef35d7ae16d744a78195f6c
talia-walkey <twrox8@gmail.com>
2017-12-06 17:11:33 -0800

    Login, register, and close account working

--- beavver.taliawalkey.ca/js/header-footer.js
+++ beavver.taliawalkey.ca/js/header-footer.js
@@ -15,29 +15,32 @@ var beforeLogin = document.getElementById("before-login"),
     signOut = document.getElementById("signOut"),
     signupBtn = document.getElementById("signup-button");
 
-signinBtn.addEventListener("click", function(){    
-    // changing the dropdown components
-    beforeLogin.style.display = "none";
-    afterLogin.style.display = "block";
-    
-    // changing the name in the login button
-    beforeLoginName.style.display = "none";
-    afterLoginName.style.display = "inline-block";
-    
-    signupBtn.style.display = "none";
-});
+if(signinBtn){
+    signinBtn.addEventListener("click", function(){    
+        // changing the dropdown components
+        beforeLogin.style.display = "none";
+        afterLogin.style.display = "block";
+        
+        // changing the name in the login button
+        beforeLoginName.style.display = "none";
+        afterLoginName.style.display = "inline-block";
+        
+        signupBtn.style.display = "none";
+    });
+}
 
-signOut.addEventListener("click", function(){
-    loginBool = false;
-    
-    // changing the dropdown components
-    beforeLogin.style.display = "block";
-    afterLogin.style.display = "none";
-    
-    // changing the name in the login button
-    beforeLoginName.style.display = "inline-block";
-    afterLoginName.style.display = "none";
-    
-    signupBtn.style.display = "block";
-});
-
+if(signOut){
+    signOut.addEventListener("click", function(){
+        loginBool = false;
+        
+        // changing the dropdown components
+        beforeLogin.style.display = "block";
+        afterLogin.style.display = "none";
+        
+        // changing the name in the login button
+        beforeLoginName.style.display = "inline-block";
+        afterLoginName.style.display = "none";
+        
+        signupBtn.style.display = "block";
+    });
+}
